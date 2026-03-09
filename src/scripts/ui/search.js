8efc94ff364156e68e7abc5f11c780f6a4092f4c
WujecDamian <wujecdamian12@gmail.com>
2026-03-10 00:16:41 +0100

    feat: add external function name called in event listener

--- src/scripts/ui/search.js
+++ src/scripts/ui/search.js
@@ -1,12 +1,14 @@
 import { locationInput } from './getLocationFromUser.js'
 import { searchWeather } from '../utils/searchWeather.js'
-
+import { updateWeatherInfo } from './getWeatherInfo.js'
 export const searchButton = document.querySelector('.search__button')
-
-searchButton.addEventListener('click', () => {
+export function search () {
   if (locationInput.value != '') {
     searchWeather(locationInput.value)
   } else {
     alert('You must give city/country name!')
   }
+}
+searchButton.addEventListener('click', () => {
+  search()
 })
