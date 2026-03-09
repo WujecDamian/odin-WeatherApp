a3f876f70cc2f86687b9018aca601fb7acd99645
WujecDamian <wujecdamian12@gmail.com>
2026-03-09 21:18:49 +0100

    feat: add imports to index

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 //put inside src/index.js
 import './styles.css'
+import { fetchWeather } from './scripts/utils/fetchWeather.js'
+import { searchButton } from './scripts/ui/search.js'
+import { locationInput } from './scripts/ui/getLocationFromUser.js'
